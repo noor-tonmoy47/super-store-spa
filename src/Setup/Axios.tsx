4b111affe3f76f20dadf7adf0b3ee75c8daa2d14
Tonmoy-7614 <noortonmoy.onnorokom@gmail.com>
2025-06-26 17:48:05 +0600

    Sending Token with each request

--- src/Setup/Axios.tsx
+++ src/Setup/Axios.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import keycloakInstance from "./keycloak";
 
 const API_BASE_URL: string = import.meta.env.PRODUCT_API_BASE_URL || 'https://localhost:7133/api/v1'
 
@@ -8,3 +9,21 @@ export const axiosInstance = axios.create({
         'Content-Type': 'application/json'
     }
 });
+
+axiosInstance.interceptors.request.use(
+  async config => {
+    // Check if Keycloak is initialized and authenticated
+    // Make sure 'keycloakInstance' is the actual Keycloak instance.
+    // If you're using a Keycloak context or hook, you'll need to get the instance from there.
+    if (keycloakInstance && keycloakInstance.authenticated && keycloakInstance.token) {
+        config.headers.Authorization = `Bearer ${keycloakInstance.token}`;
+    } else{
+        keycloakInstance.logout();
+    }
+    return config;
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
